5cc8e1326f4ca650e0e97e4969878dc4b5305e4d
“Salil48” <salilchharia@gmail.com>
2022-12-29 14:58:20 +0530

    Welcome to Day6 Practice Problems

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Greeting Message");
+
+            //FibonacciSeries.Check();
+            PerfectNumber.Check();
+            //PrimeNumber.Check();
+            //ReverseNumber.Check();
+            //StopWatch.Check();
+            //CouponNumbers.Check();
+        }
+    }     
+}
+
