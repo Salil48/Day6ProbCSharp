230548ae9e41ea864375266473eaca4d75d85775
“Salil48” <salilchharia@gmail.com>
2022-12-29 15:05:54 +0530

    Reverse Number Check

---
+++ ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class ReverseNumber
+    {
+        public static void Check()
+        {
+        
+            Console.WriteLine("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int rev = 0;
+
+            while (n != 0)
+            {
+                int rem = n % 10;
+
+                rev = rev * 10 + rem;
+
+                n = n / 10;
+
+            }
+            Console.WriteLine("Reversed Number: " + rev);
+        }
+    }
+}
